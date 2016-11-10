1aacf0a59c65909139445ecc1254d110f99e04e2
Brent Timmermans <brent.timmermans@student.odisee.be>
2016-11-10 13:24:34 +0100

    Remove vardumps

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -9,8 +9,6 @@ class EventsController extends Controller
     public function index() {
     	$json = file_get_contents('http://gturnquist-quoters.cfapps.io/api/random');
     	$data = json_decode($json, TRUE);
-    	var_dump(gettype($data));
-    	var_dump($data);
     	return view('events.index', ['data' => $data]);
     }
 
