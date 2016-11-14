8f6eea52ca1c3b07466c568f563d587b2c7f8136
Alessio De Groote <alessio.de.groote@gmail.com>
2016-11-14 11:03:42 +0100

    started on detail event page

--- resources/views/events/detail.blade.php
+++ resources/views/events/detail.blade.php
@@ -13,16 +13,35 @@
 		<p class="lead">Here will be the most amazing and well thought off description evah!</p>
 		<hr class="my-2">
 		<p><i class="fa fa-map-marker" aria-hidden="true"></i> Gebroeders De Smetstraat 1, 9000 Ghent, Belgium</p>
+		<p><i class="fa fa-calendar-check-o" aria-hidden="true"></i> From ...</p>
+		<p><i class="fa fa-calendar-check-o" aria-hidden="true"></i> Till ...</p>
 		<!-- <p class="lead"><i class="fa fa-map-marker" aria-hidden="true"></i> Gebroeders De Smetstraat 1, 9000 Ghent, Belgium</p>
 		<time datetime="2014-09-20" class="icon">
 			<em>Tuesday</em>
 			<strong>November</strong>
 			<span>15</span>
 		</time> -->
+
 	</div>
+</div>
+<div class="float-xs-right">
+	<button type="button" class="btn btn-outline-primary"><i class="fa fa-calendar-plus-o" aria-hidden="true"></i> suggest date</button>
+	<button type="button" class="btn btn-outline-primary"><i class="fa fa-map-marker" aria-hidden="true"></i> suggest location</button>
 </div>
 
 <div class="container">
+
+	<h2>Invitees</h2>
+	<ul>
+		<li>Brent Timmermans</li>
+		<li>Brent Timmermans</li>
+		<li>Brent Timmermans</li>
+		<li>Brent Timmermans</li>
+	</ul>
+
+	<h2>Requirements</h2>
+
+
 	<!-- <p><i class="fa fa-map-marker" aria-hidden="true"></i> Gebroeders De Smetstraat 1, 9000 Ghent, Belgium</p> -->
 	<time datetime="2014-09-20" class="icon">
 		<em>Tuesday</em>
