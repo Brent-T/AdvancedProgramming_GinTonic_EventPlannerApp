5a319b541bb6723df750fede2d03f6a72a9580fd
Alessio De Groote <alessio.de.groote@gmail.com>
2016-11-19 22:32:19 +0100

    cleaned code and added comments

--- resources/views/events/detail.blade.php
+++ resources/views/events/detail.blade.php
@@ -112,34 +112,34 @@
 				<h4 class="modal-title" id="myModalLabel">Suggest another date</h4>
 			</div>
 			<div class="modal-body">
-		<div class="row">
-			<div class="col-xs-12 col-sm-6 col-md-6 form-group">
-					{!! Form::label('date_start', 'Start date:') !!}
-					{!! Form::date('date_start', '', ['class' => 'form-control', 'min' => '0']) !!}
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-6 form-group">
-					{!! Form::label('time_start', 'Start time:') !!}
-					{!! Form::time('time_start', '', ['class' => 'form-control', 'min' => '0']) !!}
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-12 col-sm-6 col-md-6 form-group">
-					{!! Form::label('date_end', 'End date:') !!}
-					{!! Form::date('date_end', '', ['class' => 'form-control', 'min' => '0']) !!}
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-6 form-group">
-					{!! Form::label('time_end', 'End time:') !!}
-					{!! Form::time('time_end', '', ['class' => 'form-control', 'min' => '0']) !!}
-			</div>
-		</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-6 col-md-6 form-group">
+							{!! Form::label('date_start', 'Start date:') !!}
+							{!! Form::date('date_start', '', ['class' => 'form-control', 'min' => '0']) !!}
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-6 form-group">
+							{!! Form::label('time_start', 'Start time:') !!}
+							{!! Form::time('time_start', '', ['class' => 'form-control', 'min' => '0']) !!}
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-6 col-md-6 form-group">
+							{!! Form::label('date_end', 'End date:') !!}
+							{!! Form::date('date_end', '', ['class' => 'form-control', 'min' => '0']) !!}
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-6 form-group">
+							{!! Form::label('time_end', 'End time:') !!}
+							{!! Form::time('time_end', '', ['class' => 'form-control', 'min' => '0']) !!}
+					</div>
+				</div>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 				{!! Form::submit('Suggest date', ['class' => 'btn btn-primary']) !!}
 				{!! Form::hidden('event_id', $event->id) !!}
 			</div>
-		{!! csrf_field() !!}
-	{!! Form::close() !!}
+			{!! csrf_field() !!}
+			{!! Form::close() !!}
 		</div>
 	</div>
 </div>
@@ -174,179 +174,123 @@
 	</div>
 </div>
 
-<!-- <div class="container"> -->
-
-	<div class="row">
-		<div class="col-xs-12 col-sm-12 col-md-4 ">
-			<h3>Invitees</h3>
-			<table class="table table-invitees">
-				<tbody>
-					<tr>
-						<td><img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}"></td>
-						<td class="text-xs-left vcenter-name">Brent Timmermans</td>
-						<td class="text-xs-left vcenter-name"><a href="" class="btn btn-outline-primary btn-sm">info</a></td>
-					</tr>
-					<tr>
-						<td><img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}"></td>
-						<td class="text-xs-left vcenter-name">Alessio De Groote</td>
-						<td class="text-xs-left vcenter-name"><a href="" class="btn btn-outline-primary btn-sm">info</a></td>
-					</tr>
-					<tr>
-						<td><img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}"></td>
-						<td class="text-xs-left vcenter-name">Dieter Deschuiteneer</td>
-						<td class="text-xs-left vcenter-name"><a href="" class="btn btn-outline-primary btn-sm">info</a></td>
-					</tr>
-					<tr>
-						<td><img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}"></td>
-						<td class="text-xs-left vcenter-name">Jonas Reyniers</td>
-						<td class="text-xs-left vcenter-name"><a href="" class="btn btn-outline-primary btn-sm">info</a></td>
-					</tr>
-					<tr>
-						<td><img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}"></td>
-						<td class="text-xs-left vcenter-name">Timothy Dewolf</td>
-						<td class="text-xs-left vcenter-name"><a href="" class="btn btn-outline-primary btn-sm">info</a></td>
-					</tr>
-				</tbody>
-			</table>
-			<!-- <div class="list-group">
-				<a href="#" class="list-group-item list-group-item-action">
-					<img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}">
-					<div class="float-xs-right"><p>Brent Timmermans</p></div>
-				</a>
-				<a href="#" class="list-group-item list-group-item-action">
-					<img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}">
-					<div class="float-xs-right">
-						<p>Dieter Deschuiteneer</p>
-					</div>
-				</a>
-				<a href="#" class="list-group-item list-group-item-action">
-					<img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}">
-					<div class="float-xs-right">
-						<p>Alessio De Groote</p>
-					</div>
-				</a>
-				<a href="#" class="list-group-item list-group-item-action">
-					<img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}">
-					<div class="float-xs-right">
-						<p>Timothy Dewolf</p>
-					</div>
-				</a>
-				<a href="#" class="list-group-item list-group-item-action">
-					<img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}">
-					<div class="float-xs-right">
-						<p>Jonas Reyniers</p>
-					</div>
-				</a>
-			</div> -->
-		
-		</div>
-		<div class="col-xs-12 col-sm-12 col-md-8">
-			<h3>Requirements</h3>
-			<table class="table table-requirements">
-				<tbody>
-					<tr>
-						<td>Gender Equal food</td>
-						<td>dafuq is this even?</td>
-					</tr>
-					<tr>
-						<td>Apache Helicopters</td>
-						<td>CHOP CHOP MOFO'S</td>
-					</tr>
-					<tr>
-						<td>Trebuchets</td>
-						<td>90 kg flaming projectiles that can be launched at 300 meter distance and accuratly hit target</td>
-					</tr>
-					<tr>
-						<td>Crusaders</td>
-						<td>Deus Vult</td>
-					</tr>
-					<tr>
-						<td>Dank Memes</td>
-						<td>Thomas the Dank engine</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+<!-- Invitees and Requirements -->
+<div class="row">
+	<div class="col-xs-12 col-sm-12 col-md-4 ">
+		<h3>Invitees</h3>
+		<table class="table table-invitees">
+			<tbody>
+				<tr>
+					<td><img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}"></td>
+					<td class="text-xs-left vcenter-name">Brent Timmermans</td>
+					<td class="text-xs-left vcenter-name"><a href="" class="btn btn-outline-primary btn-sm">info</a></td>
+				</tr>
+				<tr>
+					<td><img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}"></td>
+					<td class="text-xs-left vcenter-name">Alessio De Groote</td>
+					<td class="text-xs-left vcenter-name"><a href="" class="btn btn-outline-primary btn-sm">info</a></td>
+				</tr>
+				<tr>
+					<td><img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}"></td>
+					<td class="text-xs-left vcenter-name">Dieter Deschuiteneer</td>
+					<td class="text-xs-left vcenter-name"><a href="" class="btn btn-outline-primary btn-sm">info</a></td>
+				</tr>
+				<tr>
+					<td><img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}"></td>
+					<td class="text-xs-left vcenter-name">Jonas Reyniers</td>
+					<td class="text-xs-left vcenter-name"><a href="" class="btn btn-outline-primary btn-sm">info</a></td>
+				</tr>
+				<tr>
+					<td><img class="invited-thumbnail" src="{{ asset('/img/defaultprofilepicture.jpg') }}"></td>
+					<td class="text-xs-left vcenter-name">Timothy Dewolf</td>
+					<td class="text-xs-left vcenter-name"><a href="" class="btn btn-outline-primary btn-sm">info</a></td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
-	<div class=row>
-		<div class="col-xs-12 col-sm-12 col-md-12">
-			<h3>Suggestions</h3>
-			<table class="table table-suggestions">
-				<tbody>
-					<tr>
-						<td>Gender Equal food</td>
-						<td><button type="button" class="btn btn-outline-primary btn-sm" data-container="body" data-toggle="popover" data-placement="bottom" data-content="dafuq is this even?">info</button></td>
-						<td><progress class="progress progress-success" value="5" max="10"></progress></td>
-						<td class="text-xs-center">
-							<button type="button" class="btn btn-outline-success btn-sm"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
-							<button type="button" class="btn btn-outline-danger btn-sm"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
-						</td>
-					</tr>
-					<tr>
-						<td>Apache Helicopters</td>
-						<td><button type="button" class="btn btn-outline-primary btn-sm" data-container="body" data-toggle="popover" data-placement="bottom" data-content="CHOP CHOP MOFO'S">info</button></td>
-						<td><progress class="progress progress-success" value="7" max="10"></progress></td>
-						<td class="text-xs-center">
-							<button type="button" class="btn btn-outline-success btn-sm"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
-							<button type="button" class="btn btn-outline-danger btn-sm"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
-						</td>
-					</tr>
-					<tr>
-						<td>Trebuchets</td>
-						<td><button type="button" class="btn btn-outline-primary btn-sm" data-container="body" data-toggle="popover" data-placement="bottom" data-content="90 kg flaming projectiles that can be launched at 300 meter distance and accuratly hit target">info</button></td>
-						<td><progress class="progress progress-success" value="1" max="10"></progress></td>
-						<td class="text-xs-center">
-							<button type="button" class="btn btn-outline-success btn-sm"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
-							<button type="button" class="btn btn-outline-danger btn-sm"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
-						</td>
-					</tr>
-					<tr>
-						<td>Crusaders</td>
-						<td><button type="button" class="btn btn-outline-primary btn-sm" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Deus Vult">info</button></td>
-						<td><progress class="progress progress-success" value="3" max="10"></progress></td>
-						<td class="text-xs-center">
-							<button type="button" class="btn btn-outline-success btn-sm"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
-							<button type="button" class="btn btn-outline-danger btn-sm"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
-						</td>
-					</tr>
-					<tr>
-						<td>Dank Memes</td>
-						<td><button type="button" class="btn btn-outline-primary btn-sm" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Thomas the Dank engine">info</button></td>
-						<td><progress class="progress progress-success" value="9" max="10"></progress></td>
-						<td class="text-xs-center">
-							<button type="button" class="btn btn-outline-success btn-sm"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
-							<button type="button" class="btn btn-outline-danger btn-sm"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+	<div class="col-xs-12 col-sm-12 col-md-8">
+		<h3>Requirements</h3>
+		<table class="table table-requirements">
+			<tbody>
+				<tr>
+					<td>Gender Equal food</td>
+					<td>dafuq is this even?</td>
+				</tr>
+				<tr>
+					<td>Apache Helicopters</td>
+					<td>CHOP CHOP MOFO'S</td>
+				</tr>
+				<tr>
+					<td>Trebuchets</td>
+					<td>90 kg flaming projectiles that can be launched at 300 meter distance and accuratly hit target</td>
+				</tr>
+				<tr>
+					<td>Crusaders</td>
+					<td>Deus Vult</td>
+				</tr>
+				<tr>
+					<td>Dank Memes</td>
+					<td>Thomas the Dank engine</td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
-	<!-- <h2>Invitees</h2>
-	<ul>
-		<li>Brent Timmermans</li>
-		<li>Brent Timmermans</li>
-		<li>Brent Timmermans</li>
-		<li>Brent Timmermans</li>
-	</ul>
-
-	<h2>Requirements</h2>
-
-
-	<p><i class="fa fa-map-marker" aria-hidden="true"></i> Gebroeders De Smetstraat 1, 9000 Ghent, Belgium</p>
-	<time datetime="2014-09-20" class="icon">
-		<em>Tuesday</em>
-		<strong>November</strong>
-		<span>15</span>
-	</time> -->
-<!-- </div> -->
-
-
-
-
-
-
-
+</div>
+<!-- Suggestions -->
+<div class=row>
+	<div class="col-xs-12 col-sm-12 col-md-12">
+		<h3>Suggestions</h3>
+		<table class="table table-suggestions">
+			<tbody>
+				<tr>
+					<td>Gender Equal food</td>
+					<td><button type="button" class="btn btn-outline-primary btn-sm" data-container="body" data-toggle="popover" data-placement="bottom" data-content="dafuq is this even?">info</button></td>
+					<td><progress class="progress progress-success" value="5" max="10"></progress></td>
+					<td class="text-xs-center">
+						<button type="button" class="btn btn-outline-success btn-sm"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
+						<button type="button" class="btn btn-outline-danger btn-sm"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
+					</td>
+				</tr>
+				<tr>
+					<td>Apache Helicopters</td>
+					<td><button type="button" class="btn btn-outline-primary btn-sm" data-container="body" data-toggle="popover" data-placement="bottom" data-content="CHOP CHOP MOFO'S">info</button></td>
+					<td><progress class="progress progress-success" value="7" max="10"></progress></td>
+					<td class="text-xs-center">
+						<button type="button" class="btn btn-outline-success btn-sm"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
+						<button type="button" class="btn btn-outline-danger btn-sm"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
+					</td>
+				</tr>
+				<tr>
+					<td>Trebuchets</td>
+					<td><button type="button" class="btn btn-outline-primary btn-sm" data-container="body" data-toggle="popover" data-placement="bottom" data-content="90 kg flaming projectiles that can be launched at 300 meter distance and accuratly hit target">info</button></td>
+					<td><progress class="progress progress-success" value="1" max="10"></progress></td>
+					<td class="text-xs-center">
+						<button type="button" class="btn btn-outline-success btn-sm"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
+						<button type="button" class="btn btn-outline-danger btn-sm"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
+					</td>
+				</tr>
+				<tr>
+					<td>Crusaders</td>
+					<td><button type="button" class="btn btn-outline-primary btn-sm" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Deus Vult">info</button></td>
+					<td><progress class="progress progress-success" value="3" max="10"></progress></td>
+					<td class="text-xs-center">
+						<button type="button" class="btn btn-outline-success btn-sm"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
+						<button type="button" class="btn btn-outline-danger btn-sm"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
+					</td>
+				</tr>
+				<tr>
+					<td>Dank Memes</td>
+					<td><button type="button" class="btn btn-outline-primary btn-sm" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Thomas the Dank engine">info</button></td>
+					<td><progress class="progress progress-success" value="9" max="10"></progress></td>
+					<td class="text-xs-center">
+						<button type="button" class="btn btn-outline-success btn-sm"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
+						<button type="button" class="btn btn-outline-danger btn-sm"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
 @endsection
 
 @section('JS')
