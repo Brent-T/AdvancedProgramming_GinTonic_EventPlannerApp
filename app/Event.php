036460bd03d924c6a8e298f08fd5655139d51e9b
Brent Timmermans <brent.timmermans@student.odisee.be>
2016-11-26 17:58:05 +0100

    Improved json to event conversion methods

--- app/Event.php
+++ app/Event.php
@@ -11,10 +11,10 @@ class Event extends Model
 
     // Properties of Event
     protected $fillable = [
-        'name', 'description', 'location', 'datetime_start', 'datetime_end', 'id'
+        'id', 'name', 'description', 'location', 'datetime_start', 'datetime_end'
     ];
 
-	public function __construct($name, $description, $location, $datetime_start, $datetime_end, $id = '1') {
+	public function __construct($name = "", $description = "", $location = "", $datetime_start = "", $datetime_end = "", $id = '1') {
 		$this->name = $name;
 		$this->description = $description;
 		$this->location = $location;
@@ -27,70 +27,63 @@ class Event extends Model
 	 *  Return all events found by webservice
 	 */
 	public static function GetAllEvents() {
-		$events = json_decode(file_get_contents(self::$url . '/events'), TRUE);
-		return self::convertToEventList($events);
+		$events = json_decode(file_get_contents(self::$url . '/events'));
+		return self::convertJsonToEventList($events);
 	}
 
+	/**
+	 *  Convert json array to array of Event array
+	 */
+    private static function convertJsonToEventList($json_list) {
+    	$events = array();
+		foreach ($json_list as $json_event) {
+			array_push($events, self::convertJsonToEvent($json_event));
+		}
+		return $events;
+    }
+
+	/**
+	 *  Convert json to Event object
+	 */
+	private static function convertJsonToEvent($json) {
+        $event = new Event();
+        if(isset($json->id)) $event->id = $json->id;
+        if(isset($json->name)) $event->name = $json->name;
+        if(isset($json->description)) $event->description = $json->description;
+        if(isset($json->location)) $event->location = $json->location;
+        if(isset($json->startDate)) $event->datetime_start = $json->startDate;
+        if(isset($json->endDate)) $event->datetime_end = $json->endDate;
+        return $event;
+    }
+
+	/**
+	 *  Return all events for a user
+	 */
 	public static function GetAllEventsForUser($user_id) {
-		$events = json_decode(file_get_contents(self::$url . '/eventbyuserid?userid=' . $user_id), TRUE);
-		return self::convertToEventList($events);
+		$events = json_decode(file_get_contents(self::$url . '/eventbyuserid?userid=' . urlencode($user_id)));
+		return self::convertJsonToEventList($events);
 	}
 
 	/**
 	 *  Return all events found by webservice
 	 */
 	public static function GetEventById($id) {
-		$event = json_decode(file_get_contents(self::$url . '/event?id=' .urlencode($id) ), TRUE);
-		return self::convertToEventObject($event);
-	}
-
-
-	/**
-	 *  Convert json array to Event object
-	 */
-	private static function convertToEventObject($data) {
-		$event = 
-			new Event(
-				$data['name'], 
-				$data['description'], 
-				$data['location'], 
-				$data['startDate'],
-				$data['endDate'],
-				$data['id']
-			);
-		return $event;
-	}
-
-	/**
-	 *  Convert json array to array of Event array
-	 */
-	private static function convertToEventList($data) {
-		$events = array();
-		foreach ($data as $event) {
-			array_push($events, new Event(
-				$event['name'], 
-				$event['description'], 
-				$event['location'], 
-				$event['startDate'],
-				$event['endDate'],
-				$event['id']
-			));
-		}
-		return $events;
+		$event = json_decode(file_get_contents(self::$url . '/event?id=' .urlencode($id)));
+		return self::convertJsonToEvent($event);
 	}
 
 	/**
 	 *  Add event using the web service
 	 */
 	public static function AddEvent($event) {
-		$json_event = self::createPostFields($event);
+		$json_event = self::createPostFieldsAddEvent($event);
 		self::pushEventToWebservice($json_event);
 	}
 
 	/**
 	 *  Convert Event object to post field string
 	 */
-	private static function createPostFields($event) {
+	private static function createPostFieldsAddEvent($event) {
 		$json = 
 			'name=' 
 			. $event->name 
@@ -118,9 +111,7 @@ class Event extends Model
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($curl, CURLOPT_POST, true);
 		curl_setopt($curl, CURLOPT_POSTFIELDS, $json_event);
-
 		curl_exec($curl);
-
 		curl_close($curl);
 	}
 }
