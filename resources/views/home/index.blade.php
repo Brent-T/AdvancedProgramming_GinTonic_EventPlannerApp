e135ba50a98c7de804b77762788a2e37484c9fec
Alessio De Groote <alessio.de.groote@gmail.com>
2016-11-29 22:31:38 +0100

    merge hoping for the best

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,11 +1,7 @@
 @extends('shared.layout')
 
 @section('CSS')
-<<<<<<< HEAD
-<link rel="stylesheet" href="{{ asset('/css/home-page.css') }}" />
-=======
-
->>>>>>> d1ed14d1bf00db0b6ecd19f03c6fc9a1a1ca4410
+<link rel="stylesheet" href="{{ asset('/css/homepage.css') }}" />
 @endsection
 
 @section('Content')
