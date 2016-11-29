72bb9769b9c701d84c09bcc6f3266ef27dbdca7f
Alessio De Groote <alessio.de.groote@gmail.com>
2016-11-29 15:46:56 +0100

    ik push wanneer ik wil Brent, deal with it

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,9 @@
 @extends('shared.layout')
 
+@section('CSS')
+<link rel="stylesheet" href="{{ asset('/css/home-page.css') }}" />
+@endsection
+
 @section('Content')
 <h1>Welcome to EventPlanner</h1>
 @endsection
