fe2421b4c35a167abc61ea22603bc7603fd5eb41
Brent Timmermans <brent.timmermans@student.odisee.be>
2016-12-01 18:19:00 +0100

    Errors working

--- resources/views/shared/errors.blade.php
+++ resources/views/shared/errors.blade.php
@@ -1,4 +1,4 @@
-@if (count($errors) > 0)
+@if (count($errors) > 0 || Session::has('error_message'))
 	<div class="alert alert-danger">
 		<strong>Whoops! Something went wrong!</strong>
 
@@ -8,6 +8,11 @@
 		@foreach ($errors->all() as $error)
 			<li>{{ $error }}</li>
 		@endforeach
+
+		<!-- Extra form errors -->
+		@if(Session::has('error_message'))
+			<li>{{ Session::get('error_message') }}</li>
+		@endif
 		</ul>
 	</div>
 @endif
